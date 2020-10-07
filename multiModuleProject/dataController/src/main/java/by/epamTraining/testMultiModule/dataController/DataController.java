caadd2a4ee87cf683415415081b80abb897c74f1
green2005 <green_2005@tut.by>
2020-10-07 23:25:44 +0300

    rename folder

--- multiModuleProject/dataController/src/main/java/by/epamTraining/testMultiModule/dataController/DataController.java
+++ multiModuleProject/dataController/src/main/java/by/epamTraining/testMultiModule/dataController/DataController.java
@@ -8,7 +8,7 @@ import java.util.stream.Collectors;
 
 public class DataController {
     private Repository repository;
-
+      //
     DataController() {
         repository = new Repository();
     }
